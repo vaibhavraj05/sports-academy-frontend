8172e34d26d7c49fc69234af758c0a60f68ba082
Vinayak <sutharvinayak28@gmail.com>
2023-05-26 22:23:02 +0530

    Chore: image name file

--- src/pages/CreateCourt/index.jsx
+++ src/pages/CreateCourt/index.jsx
@@ -44,12 +44,12 @@ export default function CreateCourt({ open, onCancel: handleCancel }) {
     const formData = new FormData();
     if (image?.length) {
       if (image[0].originFileObj) {
-        formData.append('files.background_image', image[0].originFileObj, image[0].name);
+        formData.append('file', image[0].originFileObj, image[0].name);
       } else {
-        formData.append('files.background_image', null);
+        formData.append('file', null);
       }
     } else {
-      data.background_image = null;
+      formData.append('file', null);
     }
     formData.append('data', JSON.stringify(data));
     createCourt(formData);
